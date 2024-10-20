330e490ad2dbf79ecd11a1a6d883be86c17aefed
kulisk <int02719@uoi.gr>
2024-10-20 18:14:56 +0300

    Camera up Down controller

--- Assets/Scripts/AnimationAndMovmentController.cs
+++ Assets/Scripts/AnimationAndMovmentController.cs
@@ -12,7 +12,6 @@ public class AnimationAndMovmentController : MonoBehaviour
     int isWalkingHash;
     int isRunningHash;
 
-
     Vector2 currentMovementInput;
     Vector2 currentLookInput;
     Vector3 currentMovement;
@@ -20,12 +19,15 @@ public class AnimationAndMovmentController : MonoBehaviour
     Vector3 currentLook;
     bool isMovementPressed;
     bool isRunPressed;
+    
     float rotationFactorPerFrame = 1.0f;
     float runMultiplier = 4.0f;
-    float mouseSensitivity = 5.0f;
+    float mouseSensitivity = 3.0f;
 
-
-
+    float cameraPitch = 0.0f; // Για τον έλεγχο της κατακόρυφης περιστροφής
+    public Transform cameraTransform; // Σύνδεση με την κάμερα του παίκτη
+    public float maxPitchAngle = 40.0f; // Όριο προς τα πάνω
+    public float minPitchAngle = -40.0f; // Όριο προς τα κάτω
 
     void Awake()
     {
@@ -41,15 +43,13 @@ public class AnimationAndMovmentController : MonoBehaviour
         PlayerInput.CharacterControls.Move.performed += onMovementInput;
         PlayerInput.CharacterControls.Run.started += onRun;
         PlayerInput.CharacterControls.Run.canceled += onRun;
-        
+
         PlayerInput.CharacterControls.Look.started += onLookInput;
         PlayerInput.CharacterControls.Look.performed += onLookInput;
         PlayerInput.CharacterControls.Look.canceled += onLookInput;
 
         Cursor.lockState = CursorLockMode.Locked;
-
     }
-
 
     void onMovementInput(InputAction.CallbackContext context)
     {
@@ -69,22 +69,30 @@ public class AnimationAndMovmentController : MonoBehaviour
 
     void handleRotation()
     {
-       float mouseX = currentLookInput.x * mouseSensitivity * Time.deltaTime;
+        // Οριζόντια περιστροφή του παίκτη (Y άξονας)
+        float mouseX = currentLookInput.x * mouseSensitivity * Time.deltaTime;
         transform.Rotate(Vector3.up * mouseX * rotationFactorPerFrame);
-    }
 
+        // Κατακόρυφη περιστροφή της κάμερας (X άξονας)
+        float mouseY = currentLookInput.y * mouseSensitivity * Time.deltaTime;
+        cameraPitch -= mouseY; // Μείωση της γωνίας για πάνω/κάτω κίνηση
+
+        // Περιορισμός της περιστροφής μεταξύ των ορίων
+        cameraPitch = Mathf.Clamp(cameraPitch, minPitchAngle, maxPitchAngle);
+
+        // Εφαρμογή της κίνησης στην κάμερα
+        cameraTransform.localEulerAngles = new Vector3(cameraPitch, 0.0f, 0.0f);
+    }
 
     void handleAnimation()
     {
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isRunning = animator.GetBool(isRunningHash);
 
-
         if (isMovementPressed && !isWalking)
         {
             animator.SetBool("isWalking", true);
         }
-
         else if (!isMovementPressed && isWalking)
         {
             animator.SetBool("isWalking", false);
@@ -98,25 +106,23 @@ public class AnimationAndMovmentController : MonoBehaviour
         {
             animator.SetBool(isRunningHash, false);
         }
-
     }
-
 
     void handleGravity()
     {
         if (characterController.isGrounded)
         {
-            float groundedGravety = -.05f;
-            currentMovement.y = groundedGravety;
-            currentRunMovement.y = groundedGravety;
+            float groundedGravity = -.05f;
+            currentMovement.y = groundedGravity;
+            currentRunMovement.y = groundedGravity;
         }
-        else{
-            float gravety = -9.8f;
-            currentMovement.y = gravety;
-            currentRunMovement.y = gravety;
+        else
+        {
+            float gravity = -9.8f;
+            currentMovement.y = gravity;
+            currentRunMovement.y = gravity;
         }
     }
-
 
     void handleMovement()
     {
@@ -134,7 +140,6 @@ public class AnimationAndMovmentController : MonoBehaviour
         }
     }
 
-
     // Update is called once per frame
     void Update()
     {
@@ -145,14 +150,13 @@ public class AnimationAndMovmentController : MonoBehaviour
 
         if (isRunPressed)
         {
-            characterController.Move( currentRunMovement * Time.deltaTime);
+            characterController.Move(currentRunMovement * Time.deltaTime);
         }
         else
         {
             characterController.Move(currentMovement * Time.deltaTime);
         }
     }
-
 
     void OnEnable()
     {
@@ -164,10 +168,3 @@ public class AnimationAndMovmentController : MonoBehaviour
         PlayerInput.CharacterControls.Disable();
     }
 }
-
-
-
-
-
-
-
